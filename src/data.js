c2047ccb54ca6f4ef2b40031db5c31e5395b300f
Hassaan Muhammad <hassaans299@gmail.com>
2024-04-13 05:32:31 -0700

    push

---
+++ src/data.js
@@ -0,0 +1,17 @@
+import Picture1 from "../public/images/p1.jpg"
+import Picture2 from "../public/images/p2.jpg"
+import Picture3 from "../public/images/p3.jpg"
+import Picture4 from "../public/images/p4.jpg"
+import Picture5 from "../public/images/p5.jpg"
+import Picture6 from "../public/images/p6.jpg"
+import Picture7 from "../public/images/p7.jpg"
+
+export{
+    Picture1,
+    Picture2,
+    Picture3,
+    Picture4,
+    Picture5,
+    Picture6,
+    Picture7
+}
